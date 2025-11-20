3517ed19b53c4718a210af4e76af4dc4359c976e
Eric Michael Martinez <eric.michael.mtz@gmail.com>
2025-11-19 22:18:28 -0600

    update

--- aistarterkit/asgi.py
+++ aistarterkit/asgi.py
@@ -12,13 +12,14 @@ import os
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
-from chat.routing import websocket_urlpatterns
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'aistarterkit.settings')
 
 # Initialize Django ASGI application early to ensure the AppRegistry
 # is populated before importing code that may import ORM models.
 django_asgi_app = get_asgi_application()
+
+from chat.routing import websocket_urlpatterns
 
 application = ProtocolTypeRouter({
     "http": django_asgi_app,
