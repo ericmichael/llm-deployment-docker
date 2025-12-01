2ca2aa488f7ba88bfa6c661c83b67efd7f0a677a
Eric Martinez <eric.michael.mtz@gmail.com>
2025-12-01 14:04:05 -0600

    update

--- chat/routing.py
+++ chat/routing.py
@@ -9,4 +9,5 @@ from . import consumers
 websocket_urlpatterns = [
     re_path(r'ws/v1/realtime$', consumers.RealtimeProxyConsumer.as_asgi()),
     re_path(r'ws/realtime$', consumers.RealtimeProxyConsumer.as_asgi()),
+    re_path(r'chat/api/v1/realtime$', consumers.RealtimeProxyConsumer.as_asgi()),
 ]
